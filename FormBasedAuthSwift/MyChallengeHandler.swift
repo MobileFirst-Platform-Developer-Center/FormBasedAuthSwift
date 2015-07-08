54f1e232d8eb12e4ebe6ca605cbf5cd99f503982
Nathan Hazout <nathanh@il.ibm.com>
2015-07-08 09:39:11 +0300

    initial commit

---
+++ FormBasedAuthSwift/MyChallengeHandler.swift
@@ -0,0 +1,63 @@
+/*
+*
+COPYRIGHT LICENSE: This information contains sample code provided in source code form. You may copy, modify, and distribute
+these sample programs in any form without payment to IBM® for the purposes of developing, using, marketing or distributing
+application programs conforming to the application programming interface for the operating platform for which the sample code is written.
+Notwithstanding anything to the contrary, IBM PROVIDES THE SAMPLE SOURCE CODE ON AN "AS IS" BASIS AND IBM DISCLAIMS ALL WARRANTIES,
+EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, ANY IMPLIED WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY,
+FITNESS FOR A PARTICULAR PURPOSE, TITLE, AND ANY WARRANTY OR CONDITION OF NON-INFRINGEMENT. IBM SHALL NOT BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR OPERATION OF THE SAMPLE SOURCE CODE.
+IBM HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS OR MODIFICATIONS TO THE SAMPLE SOURCE CODE.
+
+*/
+import Foundation
+import IBMMobileFirstPlatformFoundation
+
+class MyChallengeHandler: ChallengeHandler {
+    
+    let vc : ViewController
+    
+    init(vc: ViewController){
+        self.vc = vc
+        super.init(realm: "SampleAppRealm")
+    }
+    
+    override func isCustomResponse(response: WLResponse!) -> Bool {
+        if response != nil && response.responseText != nil{
+            if response.responseText.lowercaseString.rangeOfString("j_security_check") != nil{
+                NSLog("Detected j_security_check string - returns true")
+                return true
+            }
+        }
+        return false
+    }
+    
+    override func handleChallenge(response: WLResponse!) {
+        NSLog("A login form should appear")
+        if self.vc.navigationController?.visibleViewController.isKindOfClass(LoginViewController) == true {
+            dispatch_async(dispatch_get_main_queue()) {
+                var loginController : LoginViewController! = self.vc.navigationController?.visibleViewController as? LoginViewController
+                loginController.errorMsg.hidden = false
+            }
+        } else {
+            self.vc.performSegueWithIdentifier("showLogin", sender: self.vc)
+            dispatch_async(dispatch_get_main_queue()) {
+                var loginController : LoginViewController! = self.vc.navigationController?.visibleViewController as? LoginViewController
+                loginController.challengeHandler = self
+                loginController.errorMsg.hidden = true
+            }
+        }
+    }
+    
+    override func onSuccess(response: WLResponse!) {
+        NSLog("Challenge succeeded")
+        self.vc.navigationController?.popViewControllerAnimated(true)
+        self.submitSuccess(response)
+    }
+    
+    override func onFailure(response: WLFailResponse!) {
+        NSLog("Challenge failed")
+        self.submitFailure(response)
+    }
+    
+}
